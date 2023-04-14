d2da83806f6efb450a2bf7d7da0b6225d04154d4
Kouseki <30703483+Katou-Kouseki@users.noreply.github.com>
2023-04-14 14:53:59 +0800

    Update helper.ts

--- src/store/modules/user/helper.ts
+++ src/store/modules/user/helper.ts
@@ -17,7 +17,7 @@ export function defaultSetting(): UserState {
     userInfo: {
       avatar: 'https://raw.githubusercontent.com/Chanzhaoyu/chatgpt-web/main/src/assets/avatar.jpg',
       name: 'You',
-      description: '你与AI的一次对话',
+      description: '我的 <a href="https://kouseki.eu.org/" class="text-blue-500" target="_blank" >主页</a>',
     },
   }
 }
