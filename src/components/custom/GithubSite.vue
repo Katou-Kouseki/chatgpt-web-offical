86d56079802a35dcd06c0d023d9b032faeed84ad
Kouseki <30703483+Katou-Kouseki@users.noreply.github.com>
2023-04-14 14:48:32 +0800

    Update GithubSite.vue

--- src/components/custom/GithubSite.vue
+++ src/components/custom/GithubSite.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="text-neutral-400">
-<span>我的</span>
-    <a href="http://kouseki.eu.org/" target="_blank" class="text-blue-500">
+    <span>我的</span>
+    <a href="https://kouseki.eu.org/" target="_blank" class="text-blue-500">
       主页
     </a>
   </div>
